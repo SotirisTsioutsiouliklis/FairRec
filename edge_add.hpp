10b851b0c9bfe77e0d16d1ff6c092c2a36c3d2c8
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2020-05-04 09:20:46 +0300

    fix branches conflicts

--- edge_add.hpp
+++ edge_add.hpp
@@ -16,6 +16,7 @@ struct edge {
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 enum class algorithm_mode {GREEDY, FAST_GREEDY, APPROX, FAST_APPROX, RANDOM, RAND_SRC, ONE_TO_ALL_G,
     ONE_TO_ALL_FG, ONE_TO_ALL_R, SRC_ST, TAR_ST};
 enum class s_select_criterion {};
@@ -23,12 +24,15 @@ enum class t_select_criterion {};
 =======
 enum class algorithm_mode {GREEDY, FAST_GREEDY, APPROX, FAST_APPROX, RANDOM, RAND_SRC, ONE_TO_ALL_G, ONE_TO_ALL_FG, ONE_TO_ALL_R};
 >>>>>>> parent of 90cc69f... sources and targets in progress
+=======
+enum class algorithm_mode {GREEDY, FAST_GREEDY, APPROX, FAST_APPROX, RANDOM, RAND_SRC, ONE_TO_ALL_G, ONE_TO_ALL_FG, ONE_TO_ALL_R, SRC_ST, TAR_ST};
+>>>>>>> parent of 63fcefe... refactoring in progress. greedy stage
 
 class Edge_addition {
     public:
         Edge_addition(graph &g, pagerank_algorithms &algs, int n_source = 10, int n_target = 100);
         void greedy_per_one(const double C=0.85, const double eps=1e-4, const int max_iter=100);
-        void greedy(const double C=0.85, const double eps=1e-4, const int max_iter=100);
+        void greedy_all(const double C=0.85, const double eps=1e-4, const int max_iter=100);
         void fast_greedy_per_one(const double C=0.85, const double eps=1e-4, const int max_iter=100);
         void fast_greedy_all(const double C=0.85, const double eps=1e-4, const int max_iter=100);
         void random_edges(int exp, const double C=0.85, const double eps=1e-4, const int max_iter=100);
@@ -42,39 +46,44 @@ class Edge_addition {
 <<<<<<< HEAD
         void source_stats(const double C=0.85, const double eps=1e-4, const int max_iter=100);
         void targets_stats(const double C=0.85, const double eps=1e-4, const int max_iter=100);
+<<<<<<< HEAD
 =======
         void source_and_targets(const double C=0.85, const double eps=1e-4, const int max_iter=100);
+=======
+>>>>>>> parent of 63fcefe... refactoring in progress. greedy stage
         //void approx();
         //void fast_approx();
         void save_logs(std::string algo_name, std::vector<step_log> log_vec);
         void save_logs_per_node(std::string algo_name, int src_node, std::vector<step_log> vec);
+<<<<<<< HEAD
 >>>>>>> parent of 90cc69f... sources and targets in progress
+=======
+>>>>>>> parent of 63fcefe... refactoring in progress. greedy stage
     
     private:
         // Get n best source nodes.
-        std::vector<int> get_best_source_nodes(int n = 10, const double C=0.85, const double eps=1e-4, const int max_iter=100);
-        // Returns n_target or max number of of possible edges to add to a source node if n_target >.
-        int get_valide_no_edges(int s_node);
-        // Get best n target nodes.
-        std::vector<int> get_best_target_nodes(int s_node, int no_targets);
-        // Save vectors.
-        void save_vector(std::string file_name, pagerank_v s_vector);
-        void save_vector(std::string file_name, std::vector<int> s_vector);
-        void save_vector(std::string file_name, std::vector<double> s_vector);
-        void save_vector(std::string file_name, std::vector<edge> s_vector);
-        void save_vector(std::string file_name, std::vector<step_log> s_vector);
+        pagerank_v get_best_source_nodes(int n = 10, const double C=0.85, const double eps=1e-4, const int max_iter=100);
         // Return values of Objective function.
         pagerank_v get_objective_val(int s_node, const double C=0.85, const double eps=1e-4, const int max_iter=100);
         double get_generalized_objective_val(double init_red_pagerank, double init_source_pagerank, pagerank_v init_red_abs_prob, pagerank_v init_src_abs_prob,
             std::vector<int> init_src_nei, std::vector<edge> new_edges);
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> parent of 63fcefe... refactoring in progress. greedy stage
         // Save Source Nodes.
         void save_source_nodes(std::string algo_name, std::vector<int> sources);
         void save_target_nodes(std::string algo_name, std::vector<int> sources);
         // Save impact.
+<<<<<<< HEAD
         void save_impact(std::vector<double> impact_v);
 >>>>>>> parent of 90cc69f... sources and targets in progress
+=======
+        void save_impact(std::string name, std::vector<double> impact_v);
+        // Save edge list.
+        void save_edge_list(std::vector<edge> edges);
+>>>>>>> parent of 63fcefe... refactoring in progress. greedy stage
 
         graph &g;
         pagerank_algorithms &algs;
