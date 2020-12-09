d0671b2ce377fd0dfb8380b39b20990d11884bb0
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2020-12-09 23:03:28 +0100

    in progress fixing remove edge

--- Code/CppFiles/graph.cpp
+++ Code/CppFiles/graph.cpp
@@ -143,13 +143,14 @@ void graph::remove_edge(const int src_node, const int dest_node) {
 			position = i;
 		}
 	}
-	nodes[src_node].out_neighbors.erase(position);
+	nodes[src_node].out_neighbors.erase(nodes[src_node].out_neighbors.begin() + position);
+
 	for (int i = 0; i < nodes[dest_node].in_neighbors.size(); i++) {
 		if (nodes[dest_node].in_neighbors[i] == src_node) {
 			position = i;
 		}
 	}
-	nodes[dest_node].in_neighbors.erase(position);
+	nodes[dest_node].in_neighbors.erase(nodes[dest_node].in_neighbors.begin() + position);
 	//nodes[src_node].out_neighbors.pop_back();
 	//nodes[dest_node].in_neighbors.pop_back();
 	--nodes[src_node].out_neighbors_per_community[nodes[dest_node].community];
