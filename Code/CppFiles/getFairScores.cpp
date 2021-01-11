1cb7a7508a216ab3906360596e0e0b069535135f
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2021-01-11 21:17:02 +0100

    ad get fair scores file

--- Code/CppFiles/getFairScores.cpp
+++ Code/CppFiles/getFairScores.cpp
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
     ifstream source_nodes(edge_file);
     getline(source_nodes, str_edge);
     while (getline(source_nodes, str_edge)) {
-        edge = split(str_edge, ",");
+        edge = split(str_edge, ',');
         source = edge[0];
         target = edge[1];
         adj_list[source].push_back(target);
