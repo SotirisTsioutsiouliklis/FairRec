b2c502f7af4f4570bdcb56328b4e3cd0de07d1e7
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2021-01-11 21:14:09 +0100

    ad get fair scores file

--- Code/CppFiles/getFairScores.cpp
+++ Code/CppFiles/getFairScores.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
     // Save results.
     ofstream edge_scores(edge_scores_file);
     edge_scores << "Sources,Targets,Scores\n";
-    for (map<int, vector<int>::iterator iter = adj_list.begin(); iter != adj_list.end(); ++iter) {
+    for (map<int, vector<int>>::iterator iter = adj_list.begin(); iter != adj_list.end(); ++iter) {
         source = iter->first;
         targets = iter->second;
         obj_values = algs.getObjectiveValues(source);
