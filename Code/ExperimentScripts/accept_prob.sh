53d99514de6b110f67205af7f87b4d24d313d97e
ksemer <konstantinos.semertzidis@gmail.com>
2021-01-15 10:19:50 +0000

    Change output files

--- Code/ExperimentScripts/accept_prob.sh
+++ Code/ExperimentScripts/accept_prob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python3 experiment_two_acceptance.py -r 10 -s node2vec_scores.csv -n node2vec_scores.csv -o accept_prob_node2vec_scores.csv
-python3 experiment_two_acceptance.py -r 10 -s adamic_adar_scores.csv -n node2vec_scores.csv -o accept_prob_adamic_adar_scores.csv
+python3 experiment_two_acceptance.py -r 10 -s node2vec_scores.csv -n node2vec_scores.csv -o accept_prob_node2vec.csv
+python3 experiment_two_acceptance.py -r 10 -s adamic_adar_scores.csv -n node2vec_scores.csv -o accept_prob_adamic_adar.csv
 python3 experiment_two_acceptance.py -r 10 -s jaccard_coefficient_scores.csv -n node2vec_scores.csv -o accept_prob_jaccard-coefficient.csv
 python3 experiment_two_acceptance.py -r 10 -s resource_allocation_scores.csv -n node2vec_scores.csv -o accept_prob_resource-allocation.csv
 python3 experiment_two_acceptance.py -r 10 -s preferential_attachment_scores.csv -n node2vec_scores.csv -o accept_prob_preferential-attachment.csv
--- Code/ExperimentScripts/fair_score.sh
+++ Code/ExperimentScripts/fair_score.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python3 experiment_one_fairness.py -r 10 -s node2vec_scores.csv -o sc_node2vec_scores.csv
-python3 experiment_one_fairness.py -r 10 -s adamic_adar_scores.csv  -o sc_adamic_adar_scores.csv
+python3 experiment_one_fairness.py -r 10 -s node2vec_scores.csv -o sc_node2vec.csv
+python3 experiment_one_fairness.py -r 10 -s adamic_adar_scores.csv  -o sc_adamic_adar.csv
 python3 experiment_one_fairness.py -r 10 -s jaccard_coefficient_scores.csv -o sc_jaccard-coefficient.csv
 python3 experiment_one_fairness.py -r 10 -s resource_allocation_scores.csv  -o sc_resource-allocation.csv
 python3 experiment_one_fairness.py -r 10 -s preferential_attachment_scores.csv  -o sc_preferential-attachment.csv
