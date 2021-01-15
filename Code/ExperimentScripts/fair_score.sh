8fbc66b6bdaec6e9a5dc922742905d1ad7636b98
ksemer <konstantinos.semertzidis@gmail.com>
2021-01-15 15:30:17 +0000

    Add hybrid

--- Code/ExperimentScripts/fair_score.sh
+++ Code/ExperimentScripts/fair_score.sh
@@ -6,3 +6,4 @@ python3 experiment_one_fairness.py -r 10 -s resource_allocation_scores.csv  -o s
 python3 experiment_one_fairness.py -r 10 -s preferential_attachment_scores.csv  -o sc_preferential-attachment.csv
 python3 experiment_one_fairness.py -r 10 -s fairwalk_scores.csv -o sc_fairwalk.csv
 python3 experiment_one_fairness.py -r 10 -s fair_scores.csv -o sc_fair.csv
+python3 experiment_one_fairness.py -r 10 -s hybrid_node2vec_scores.csv -o sc_hybrid.csv
