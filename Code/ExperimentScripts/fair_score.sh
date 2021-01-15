a78f580392c7ce837c330a66ceb3739931b02330
ksemer <konstantinos.semertzidis@gmail.com>
2021-01-15 10:04:29 +0000

    Create fair_score.sh

---
+++ Code/ExperimentScripts/fair_score.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 experiment_one_fairness.py -r 10 -s node2vec_scores.csv -o sc_node2vec_scores.csv
+python3 experiment_one_fairness.py -r 10 -s adamic_adar_scores.csv  -o sc_adamic_adar_scores.csv
+python3 experiment_one_fairness.py -r 10 -s jaccard_coefficient_scores.csv -o sc_jaccard-coefficient.csv
+python3 experiment_one_fairness.py -r 10 -s resource_allocation_scores.csv  -o sc_resource-allocation.csv
+python3 experiment_one_fairness.py -r 10 -s preferential_attachment_scores.csv  -o sc_preferential-attachment.csv
+python3 experiment_one_fairness.py -r 10 -s fairwalk_scores.csv -o sc_fairwalk.csv
+python3 experiment_one_fairness.py -r 10 -s fair_scores.csv -o sc_fair.csv
