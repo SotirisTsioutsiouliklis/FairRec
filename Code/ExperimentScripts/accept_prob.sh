177c53114f84b84bdf3e750053f209c9cdaff62f
ksemer <konstantinos.semertzidis@gmail.com>
2021-01-15 09:42:41 +0000

    Update accept_prob.sh

--- Code/ExperimentScripts/accept_prob.sh
+++ Code/ExperimentScripts/accept_prob.sh
@@ -4,3 +4,4 @@ python3 experiment_two_acceptance.py -r 10 -s adamic_adar_scores.csv -n node2vec
 python3 experiment_two_acceptance.py -r 10 -s jaccard_coefficient_scores.csv -n node2vec_scores.csv -o accept_prob_jaccard-coefficient.csv
 python3 experiment_two_acceptance.py -r 10 -s resource_allocation_scores.csv -n node2vec_scores.csv -o accept_prob_resource-allocation.csv
 python3 experiment_two_acceptance.py -r 10 -s preferential_attachment_scores.csv -n node2vec_scores.csv -o accept_prob_preferential-attachment.csv
+python3 experiment_two_acceptance.py -r 10 -s fairwalk_scores.csv -n node2vec_scores.csv -o accept_prob_fairwalk.csv
