78a48293ad8198d39ed881ca9b82bd0853fa25b8
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2021-02-11 20:02:33 +0100

    build source node schema

--- Code/PythonFiles/getSourceNodes.py
+++ Code/PythonFiles/getSourceNodes.py
@@ -18,10 +18,35 @@ Examples:
 
             >>> python3 getSourceNodes.py -p random -a 10 -o random_source_nodes.csv
 
-        2. pagerank:
-            >>> python3 getSourceNodes.py -p pagerank -a <percentage_of_nodes> [-o <output_file>]
+        2. best-pagerank:
+            >>> python3 getSourceNodes.py -p best-pagerank -a <percentage_of_nodes> [-o <output_file>]
 
-            >>> python3 getSourceNodes.py -p pagerank -a 10 -o random_source_nodes.csv
+            >>> python3 getSourceNodes.py -p best-pagerank -a 10 -o random_source_nodes.csv
+
+        3. worst-pagerank:
+            >>> python3 getSourceNodes.py -p worst-pagerank -a <percentage_of_nodes> [-o <output_file>]
+
+            >>> python3 getSourceNodes.py -p worst-pagerank -a 10 -o random_source_nodes.csv
+
+        4. best-degree:
+            >>> python3 getSourceNodes.py -p best-degree -a <percentage_of_nodes> [-o <output_file>]
+
+            >>> python3 getSourceNodes.py -p best-degree -a 10 -o random_source_nodes.csv
+
+        5. worst-degree:
+            >>> python3 getSourceNodes.py -p worst-degree -a <percentage_of_nodes> [-o <output_file>]
+
+            >>> python3 getSourceNodes.py -p worst-degree -a 10 -o random_source_nodes.csv
+
+        6. random-red:
+            >>> python3 getSourceNodes.py -p random-red -a <percentage_of_nodes> [-o <output_file>]
+
+            >>> python3 getSourceNodes.py -p random-red -a 10 -o random_source_nodes.csv
+
+        7. random-blue:
+            >>> python3 getSourceNodes.py -p random-blue -a <percentage_of_nodes> [-o <output_file>]
+
+            >>> python3 getSourceNodes.py -p random-blue -a 10 -o random_source_nodes.csv
 
 Notes:
     1. Additional arguments should be given in the specified order,
@@ -95,13 +120,49 @@ def getBestByPagerankSourceNodes(percentage: int, output_file: str = None):
 
     # Set the name of the output file.
     if output_file is None:
-        output_file = f"pagerank_source_nodes_{percentage}.csv"
+        output_file = f"best_pagerank_source_nodes_{percentage}.csv"
     w = open(output_file, "w")
     w.write("Nodes\n")
 
     for i in sorted_nodes:
         w.write(f"{i}\n")
     w.close()
+
+
+def getWorstByPagerankSourceNodes(percentage: int, output_file: str = None):
+    # Read pagerank.
+    pagerank = pd.read_csv("", sep="\t")["Pagerank"].to_numpy()
+    sorted_nodes = np.argsort(pagerank)
+    # Keep 10% of them.
+    number_of_nodes = pagerank.size
+    number_of_source_nodes = int((number_of_nodes * percentage) // 100)
+    sorted_nodes = sorted_nodes[0:number_of_source_nodes]
+
+    # Set the name of the output file.
+    if output_file is None:
+        output_file = f"worst_pagerank_source_nodes_{percentage}.csv"
+    w = open(output_file, "w")
+    w.write("Nodes\n")
+
+    for i in sorted_nodes:
+        w.write(f"{i}\n")
+    w.close()
+
+
+def getBestByDegree(percentage: int, output_file: str = None):
+    pass
+
+
+def getWorstByDegree(percentage: int, output_file: str = None):
+    pass
+
+
+def getRandomRed(percentage: int, output_file: str = None):
+    pass
+
+
+def getRandomBlue(percentage: int, output_file: str = None):
+    pass
 
 
 # Adjust input check according to the new features added.
@@ -133,7 +194,7 @@ if __name__ == "__main__":
                     InputErrors.argumentError()
             else:
                 getRandomSourceNodes(percentage)
-    elif sys.argv[2] == "pagerank":
+    elif sys.argv[2] == "best-pagerank":
         percentage = float(sys.argv[4])
         # Check for valid percentage input.
         if not (percentage > 0 and percentage < 100):
@@ -147,6 +208,76 @@ if __name__ == "__main__":
                     InputErrors.argumentError()
             else:
                 getBestByPagerankSourceNodes(percentage)
+    elif sys.argv[2] == "worst-pagerank":
+        percentage = float(sys.argv[4])
+        # Check for valid percentage input.
+        if not (percentage > 0 and percentage < 100):
+            InputErrors.percentage_error()
+        else:
+            if len(sys.argv) == 7:
+                if sys.argv[5] == "-o":
+                    output_file = sys.argv[6]
+                    getRandomSourceNodes(percentage, output_file)
+                else:
+                    InputErrors.argumentError()
+            else:
+                getWorstByPagerankSourceNodes(percentage)
+    elif sys.argv[2] == "best-degree":
+        percentage = float(sys.argv[4])
+        # Check for valid percentage input.
+        if not (percentage > 0 and percentage < 100):
+            InputErrors.percentage_error()
+        else:
+            if len(sys.argv) == 7:
+                if sys.argv[5] == "-o":
+                    output_file = sys.argv[6]
+                    getRandomSourceNodes(percentage, output_file)
+                else:
+                    InputErrors.argumentError()
+            else:
+                getWorstByPagerankSourceNodes(percentage)
+    elif sys.argv[2] == "worst-degree":
+        percentage = float(sys.argv[4])
+        # Check for valid percentage input.
+        if not (percentage > 0 and percentage < 100):
+            InputErrors.percentage_error()
+        else:
+            if len(sys.argv) == 7:
+                if sys.argv[5] == "-o":
+                    output_file = sys.argv[6]
+                    getRandomSourceNodes(percentage, output_file)
+                else:
+                    InputErrors.argumentError()
+            else:
+                getWorstByPagerankSourceNodes(percentage)
+    elif sys.argv[2] == "random-red":
+        percentage = float(sys.argv[4])
+        # Check for valid percentage input.
+        if not (percentage > 0 and percentage < 100):
+            InputErrors.percentage_error()
+        else:
+            if len(sys.argv) == 7:
+                if sys.argv[5] == "-o":
+                    output_file = sys.argv[6]
+                    getRandomSourceNodes(percentage, output_file)
+                else:
+                    InputErrors.argumentError()
+            else:
+                getWorstByPagerankSourceNodes(percentage)
+    elif sys.argv[2] == "random-blue":
+        percentage = float(sys.argv[4])
+        # Check for valid percentage input.
+        if not (percentage > 0 and percentage < 100):
+            InputErrors.percentage_error()
+        else:
+            if len(sys.argv) == 7:
+                if sys.argv[5] == "-o":
+                    output_file = sys.argv[6]
+                    getRandomSourceNodes(percentage, output_file)
+                else:
+                    InputErrors.argumentError()
+            else:
+                getWorstByPagerankSourceNodes(percentage)
     # Handles all other incorrect inputs.
     else:
         InputErrors.argumentError()
