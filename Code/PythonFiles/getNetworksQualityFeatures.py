1ee0a8bbbe04d34f9b1f35b27e64c4967a574339
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2021-03-30 11:49:38 +0300

    add quality feature scripts. Reformated with black all python scripts

---
+++ Code/PythonFiles/getNetworksQualityFeatures.py
@@ -0,0 +1,133 @@
+""" Calculates the networks quality features.
+Quality features: #nodes, #edges, homophily,
+degree distribution (in -out), red ratio, red pagerank. 
+"""
+import numpy as np
+import networkx as nx
+import time
+import sys
+
+
+def getNodeCommunities():
+    """Returns dictionary with node communities.
+    Read out_communtiy.txt and creates a dictionary with key the nodes
+    ids and arguments the nodes community.
+    """
+    nodeCommunities = dict()
+    with open("out_community.txt", "r") as fileOne:
+        fileOne.readline()
+        for line in fileOne:
+            node = int(line.split()[0])
+            community = int(line.split()[1])
+            nodeCommunities[node] = {"community": community}
+
+    return nodeCommunities
+
+
+def getRedRatio(graph):
+    """Return the ratio of red nodes in graph."""
+    numberOfRedNodes = 0
+    numberOfNodes = graph.number_of_nodes()
+    communities = nx.get_node_attributes(graph, "community")
+
+    for i in range(numberOfNodes):
+        if communities[i] == 1:
+            numberOfRedNodes += 1
+
+    redRatio = numberOfRedNodes / numberOfNodes
+
+    return redRatio
+
+
+def getHomophily(graph):
+    """Return homophily of the graph.
+    Homophily = cross edges ratio / expected cross edges ratio.
+    homophily = 1 means no homophily. homophily > 1 means heterohily.
+    homphily < 1 means homophily.
+    """
+    global redRatio
+    blueRatio = 1 - redRatio
+    crossEdges = 0
+
+    communities = nx.get_node_attributes(graph, "community")
+
+    for edge in graph.edges:
+        if communities[edge[0]] != communities[edge[1]]:
+            crossEdges += 1
+
+    crossEdgesRatio = crossEdges / graph.number_of_edges()
+
+    expectedCrossEdges = 2 * redRatio * blueRatio
+    homophily = crossEdgesRatio / expectedCrossEdges
+
+    return homophily
+
+
+def getInDegreeDistribution(graph):
+    inDegrees = list(graph.in_degree())
+    inDegreesArray = np.array([inDegree for node, inDegree in inDegrees])
+    inDegreesDistribution = np.histogram(inDegreesArray, bins=max(inDegreesArray) + 1)
+
+    return inDegreesDistribution[0]
+
+
+def getOutDegreeDistribution(graph):
+    outDegrees = list(graph.out_degree())
+    outDegreesArray = np.array([outDegree for node, outDegree in outDegrees])
+    outDegreesDistribution = np.histogram(
+        outDegreesArray, bins=max(outDegreesArray) + 1
+    )
+
+    return outDegreesDistribution[0]
+
+
+def getRedPagerank():
+    """Calculates the redPagerank of the network."""
+    pagerank = np.loadtxt("out_pagerank.txt", skiprows=1)[:, 1]
+    communities = np.loadtxt("out_community.txt", skiprows=1, dtype=int)
+
+    redPagerank = 0.0
+    for i in range(pagerank.size):
+        if communities[i][1] == 1:
+            redPagerank += pagerank[communities[i][0]]
+
+    return redPagerank
+
+
+# ------------------------------------------------------------------------------------
+# Start timing.
+startTime = time.time()
+
+# Load graph.
+graph = nx.read_edgelist("out_graph.txt", nodetype=int, create_using=nx.DiGraph())
+
+# Read node attributes.
+nodeAttributes = getNodeCommunities()
+nx.set_node_attributes(graph, nodeAttributes)
+
+# Get red ratio.
+redRatio = getRedRatio(graph)
+
+# Store results.
+with open("networksQualityFeatures.txt", "w") as fileOne:
+    fileOne.write("NumberOfNodes\t%d\n" % graph.number_of_nodes())
+    fileOne.write("NumberOfEdges\t%d\n" % graph.number_of_edges())
+    fileOne.write("RedRatio\t%f\n" % redRatio)
+    fileOne.write("RedPagerank\t%f\n" % getRedPagerank())
+    fileOne.write("Homophily\t%f\n" % getHomophily(graph))
+
+# Get in degree distribution.
+inDegreeDistribution = getInDegreeDistribution(graph)
+outDegreeDistribution = getOutDegreeDistribution(graph)
+
+# Store distributions.
+np.savetxt("inDegreeDistribution.txt", inDegreeDistribution, delimiter="\t")
+np.savetxt("outDegreeDistribution.txt", outDegreeDistribution, delimiter="\t")
+
+# Stop timing
+stopTime = time.time()
+
+# Log time.
+elapsedTime = stopTime - startTime
+with open("networksQualityFeatures_timing.txt", "w") as fileOne:
+    fileOne.write("Elapsed time:\t%f seconds" % elapsedTime)
---
+++ Code/PythonFiles/getNodesQualityFeatures.py
@@ -0,0 +1,104 @@
+""" Calculates nodes quality features.
+Quality features: pagerank, red pagerank, group, degree (in-out),
+red neighbors ratio.
+"""
+import numpy as np
+import networkx as nx
+import time
+import sys
+
+
+def getNodesRedPagerank(pagerank, communities):
+    """Reads the redPagerank of all nodes and store it."""
+    redPagerank = np.loadtxt("out_personilized_red.txt", skiprows=1, usecols=1)
+
+    return redPagerank
+
+
+def getRedInNeighbors(graph, node):
+    redInNeighbors = np.zeros(graph.number_of_nodes(), dtype=int)
+
+    for i in range(graph.number_of_nodes()):
+        inNeighbors = [nei for nei in graph.predecessors(i) if communities[nei] == 1]
+        redInNeighbors[i] = len(inNeighbors)
+
+    return redInNeighbors
+
+
+def getRedOutNeighbors(graph, node):
+    redOutNeighbors = np.zeros(graph.number_of_nodes(), dtype=int)
+    for i in range(graph.number_of_nodes()):
+        outNeighbors = [
+            nei for nei in graph.neighbors(i) if communities[nei] == 1
+        ]  # .neighbors equal to .successors for digraphs.
+        redOutNeighbors[i] = len(outNeighbors)
+
+    return redOutNeighbors
+
+
+def getInRedNeighborRatio(graph, communities):
+    redInNeighbors = getRedInNeighbors(graph, communities)
+    redInNeighborhoodRatio = [
+        redInNeighbors[i] / graph.in_degree(i) if graph.in_degree(i) != 0 else 0.5
+        for i in range(graph.number_of_nodes())
+    ]
+
+    return redInNeighborhoodRatio
+
+
+def getOutRedNeighborRatio(graph, communities):
+    redOutNeighbors = getRedOutNeighbors(graph, communities)
+    redOutNeighborhoodRatio = [
+        redOutNeighbors[i] / graph.out_degree(i) if graph.out_degree(i) != 0 else 0.5
+        for i in range(graph.number_of_nodes())
+    ]
+
+    return redOutNeighborhoodRatio
+
+
+# ---------------------------------------------------------------
+# Start timing.
+startTime = time.time()
+
+# Load graph.
+graph = nx.read_edgelist("out_graph.txt", nodetype=int, create_using=nx.DiGraph())
+# Get pagerank.
+pagerank = np.loadtxt("out_pagerank.txt", skiprows=1)[:, 1]
+# Get communities. TODO: improve.
+preCommunities = np.loadtxt("out_community.txt", skiprows=1, dtype=int)
+communities = np.zeros(graph.number_of_nodes())
+for i in range(graph.number_of_nodes()):
+    communities[preCommunities[i][0]] = preCommunities[i][1]
+# Get red pagerank.
+redPagerank = getNodesRedPagerank(pagerank, communities)
+# Get red neighborhood ratio.
+redInNeighborsRatio = getInRedNeighborRatio(graph, communities)
+redOutNeighborsRatio = getOutRedNeighborRatio(graph, communities)
+
+# Store results.
+with open("nodeQualityFeatures.txt", "w") as fileOne:
+    fileOne.write(
+        "nodeId\tpagerank\tredPagerank\tgroup\tinDegree\toutDegree\tredNeighborsInRatio\tredNeighborsOutRatio\n"
+    )
+    for i in range(graph.number_of_nodes()):
+        fileOne.write(
+            "%d\t%f\t%f\t%d\t%d\t%d\t%f\t%f\n"
+            % (
+                i,
+                pagerank[i],
+                redPagerank[i],
+                communities[i],
+                graph.in_degree(i),
+                graph.out_degree(i),
+                redInNeighborsRatio[i],
+                redOutNeighborsRatio[i],
+            )
+        )
+
+# Stop timing.
+stopTime = time.time()
+
+# Log time.
+elapsedTime = stopTime - startTime
+with open("nodeQualityFeatures-timing.txt", "w") as fileOne:
+    fileOne.write("Total time:\t%f seconds" % elapsedTime)
